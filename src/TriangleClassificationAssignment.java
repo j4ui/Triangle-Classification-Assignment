ad8f31b738800c6ec286b24a30240065111d1e0b
Underio3298 <underio3298@gmail.com>
2026-04-22 10:41:43 -0400

    Final touches and change jdk to 21

--- src/TriangleClassificationAssignment.java
+++ src/TriangleClassificationAssignment.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class TriangleClassificationAssignment {
     public static void main(String[] args) {
+        triangleClassification();
+    }
+
+    public static void triangleClassification() {
         Scanner scanner = new Scanner(System.in);
         int a, b, c, max;
         do {
@@ -9,41 +13,32 @@ public class TriangleClassificationAssignment {
             a = scanner.nextInt();
             b = scanner.nextInt();
             c = scanner.nextInt();
-            // set c to the longest side
             if (a > b) {max = a; a = b; b = max;}
             if (b > c) {max = b; b = c; c = max;}
             if (a > b) {max = a; a = b; b = max;}
-            if (a == 0 && b == 0 && c == 0) {System.out.println(a + " " + b + " " + c + " Program was terminated by user."); break;}
-            if (isValidTriangle(a, b, c)) {
-                System.out.println(a + " " + b + " " + c + " Triangle possible: " + triangleType(a, b, c) + " and " + angleType(a, b, c) + " with the following angles: \n" + angles(a, b, c));
-            } else {
-                System.out.println(a + " " + b + " " + c + " Triangle cannot be formed.");
-            }
-        } while (a != 0 && b != 0 && c != 0);
+            output(a, b, c);
+        } while (a != 0 || b != 0 || c != 0);
+    }
+
+    public static void output(int a, int b, int c) {
+        if (a == 0 && b == 0 && c == 0) {System.out.println(a + " " + b + " " + c + " Program was terminated by user."); return;}
+        if (isValidTriangle(a, b, c)) {
+            System.out.println(a + " " + b + " " + c + " Triangle possible: " + triangleType(a, b, c) + " and " + angleType(a, b, c) + " with the following angles: \n" + angles(a, b, c));
+        } else {System.out.println(a + " " + b + " " + c + " Triangle cannot be formed.");}
     }
 
     public static String triangleType(int a, int b, int c) {
-        if (a == b && b == c && a == c) {
-            return "equilateral";
-        } else if (a == b || b == c || a == c) {
-            return "isosceles";
-        } else {
-            return "scalene";
-        }
+        if (a == b && b == c && a == c) {return "equilateral";}
+        else if (a == b || b == c || a == c) {return "isosceles";}
+        else {return "scalene";}
     }
 
-    public static boolean isValidTriangle(int a, int b, int c) {
-        return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
-    }
+    public static boolean isValidTriangle(int a, int b, int c) {return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;}
 
     public static String angleType(int a, int b, int c) {
-        if (a*a + b*b == c*c) {
-            return "right";
-        } else if (a*a + b*b > c*c) {
-            return "acute";
-        } else {
-            return "obtuse";
-        }
+        if (a*a + b*b == c*c) {return "right";}
+        else if (a*a + b*b > c*c) {return "acute";}
+        else {return "obtuse";}
     }
 
     public static String angles(int a, int b, int c) {
